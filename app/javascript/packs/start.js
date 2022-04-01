013d05bc303a75830e29f58a8c0c1b8dd153060c
israr-hussain117 <israr@thesoftaims.com>
2022-04-01 19:36:54 +0500

    defined jquery in enviormentjs

--- app/javascript/packs/start.js
+++ app/javascript/packs/start.js
@@ -2,7 +2,7 @@
 class Start {
   constructor() {
     var CI = this;
-		$(".btn-blue").click(function(e) {
+		$(".btn-blue").click(function() {
 		  $("html, body").animate({ scrollTop: 0 }, "slow");
 		  return false;
 		});
