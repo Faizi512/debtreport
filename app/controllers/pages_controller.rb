6c8a57f84397a0072693ff5003be6c979d07df94
HashirRehmanKhan <hashir.rehman@biomarking.com>
2022-05-23 23:07:56 +0500

    Routes set

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,33 +1,10 @@
 class PagesController < ApplicationController
-	include PagesHelper
-	def index
-		home
-	end
-	def success
 
-	end
-	def privacy_policy
-
-	end
-	def terms_and_conditions
-
-	end
-	def cookies
-
-	end
 	def start
 
 	end
 
-	def new
 
-	end
-
-	def home_cookie_policy
-	end
-
-	def home_privacy_policy
-	end
 
 
 end
