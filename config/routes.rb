efdd57894ce100b41e53a46c56334104642443ab
HashirRehmanKhan <hashir.rehman@biomarking.com>
2022-05-23 22:38:21 +0500

    Dept report start page

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get '/start', to: 'pages#start'
-  get '/new', to: 'pages#new'
-  get '/sms', to: 'pages#sms'
-  get '/success', to: 'pages#success'
-  get '/cookies', to: 'pages#cookies'
-  get '/home-cookies', to: 'pages#home_cookie_policy'
-  get '/home-privacy', to: 'pages#home_privacy_policy'
-  get '/privacy-policy', to: 'pages#privacy_policy'
-  get '/terms-and-conditions', to: 'pages#terms_and_conditions'
 end
